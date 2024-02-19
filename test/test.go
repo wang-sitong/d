2580cf892ea5e470b761e2eee93b232d4d8f03f0
wangsitong <512350703@qq.com>
2024-02-19 13:52:23 +0800

    v0.0.1

---
+++ test/test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"time"
+)
+
+func main() {
+	//d := date("Y-m-d H:i:s", "now")
+	//d := date("Y-m-d", "now")
+	d := date("Y-m-d H:i:s", -2)
+	panic(d)
+}
+
+func date(params ...interface{}) string {
+	// 定义默认值
+	defaultValues := map[int]interface{}{
+		0: "Y-m-d H:i:s", // format
+		1: 0,             // date
+		2: "UTC+8",       // date
+
+	}
+	defaultCount := len(defaultValues)
+	count := len(params)
+	value := defaultCount - count
+	if value > 0 {
+		for i := defaultCount - value; i < defaultCount; i++ {
+			params = append(params, defaultValues[i])
+		}
+	}
+
+	format := params[0]
+	day := params[1].(int)
+	result := "format error"
+	currentDateTime := time.Unix(time.Now().Unix()+int64(86400*day), 0)
+	switch format {
+	case "Y-m-d H:i:s":
+		result = currentDateTime.Format("2006-01-02 15:04:05")
+		break
+	case "Y-m-d":
+		result = currentDateTime.Format("2006-01-02")
+		break
+	case "H:i:s":
+		result = currentDateTime.Format("15:04:05")
+		break
+	default:
+		result = "format error"
+	}
+
+	return result
+}
